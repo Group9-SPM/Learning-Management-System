82f2589d90d3127c2ac6672dc6bb569c15ba61d2
Diyanah <diyanahsoh@gmail.com>
2021-10-19 15:37:23 +0800

    added testing code on form submission

--- docker/quiz.py
+++ docker/quiz.py
@@ -30,8 +30,7 @@ class Quiz(db.Model):
     quizDuration = db.Column(db.String(20), nullable=False)
     passingCriteria = db.Column(db.String(5), nullable=False)
     quizType = db.Column(db.String(2), nullable=False)
-    lessonID = db.Column(db.String(1), nullable=False)
-
+    lessonID = db.Column(db.Integer, nullable=False)
 
     def __init__(self, quizID, quizDuration, passingCriteria, quizType, lessonID):
         self.quizID = quizID
@@ -39,6 +38,21 @@ class Quiz(db.Model):
         self.passingCriteria = passingCriteria
         self.quizType = quizType
         self.lessonID = lessonID
+class Questions(db.Model):
+    __tablename__ = 'quizQuestions'
+
+    quizID = db.Column(db.Integer, primary_key=True)
+    qnNo = db.Column(db.Integer, nullable=False)
+    question = db.Column(db.String(300), nullable=False)
+    options = db.Column(db.String(100), nullable=False)
+    answer = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, quizID, qnNo, question, options, answer):
+        self.quizID = quizID
+        self.qnNo = qnNo
+        self.question = question
+        self.options = options
+        self.answer = answer
 
 
 @app.route("/quiz")
@@ -60,6 +74,41 @@ def get_all():
         }
     ), 404
 
+@app.route('/quiz/create', methods=['POST'])
+def create_quiz():
+    data = request.get_json()
+
+    # if ( request.get_json() is not None ):
+    #     quizCreation = Questions(**data)
+    #     print("h1")
+    #     try:
+    #         db.session.add(quizCreation)
+    #         db.session.commit()
+    #     except Exception as ex:
+    #         db.session.rollback()
+    #         return jsonify({"message": "An error occurred creating quiz."}), 500
     
+    #     return jsonify(quizCreation.json()), 201
+
+    # read data from the form and save in variable
+    question = request.form['question']
+    options = request.form['options']
+    answer = request.form['answer']
+
+    # store in database
+    try:
+        con = sql.connect('qa_database.db')
+        c =  con.cursor() # cursor
+        # insert data
+        c.execute("INSERT INTO quizQuestions (question, options, answer) VALUES (?,?,?)",
+            (question, options, answer))
+        con.commit() # apply changes
+        # go to thanks page
+        return render_template('createThanks.html', question=question)
+    except con.Error as err: # if error
+        # then display the error in 'database_error.html' page
+        return render_template('database_error.html', error=err)
+    finally:
+        con.close() # close the connection
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5007, debug=True)
